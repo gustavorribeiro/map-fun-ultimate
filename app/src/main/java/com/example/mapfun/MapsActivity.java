27f455ce542633a62dfb679efc3a219df63ce93a
Gustavo Ribeiro <gustavo.mribeiroxg@yahoo.com>
2023-01-27 13:00:44 -0300

    Mount Everest location feature

--- app/src/main/java/com/example/mapfun/MapsActivity.java
+++ app/src/main/java/com/example/mapfun/MapsActivity.java
@@ -44,8 +44,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(27.988461, 86.924889);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Mount Everest, biggest in the world"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
